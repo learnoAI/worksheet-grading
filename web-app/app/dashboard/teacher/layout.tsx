7e96dcb1b66e42e47bddee80c4ca18b1675ffcb7
ayam04 <ayamullahkhan04@gmail.com>
2025-11-13 18:53:14 +0530

    fix redirect

--- web-app/app/dashboard/teacher/layout.tsx
+++ web-app/app/dashboard/teacher/layout.tsx
@@ -1,12 +1,25 @@
 'use client';
 
 import { TeacherLayout } from '@/src/components/layout/TeacherLayout';
+import { usePathname, useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
+const teacher_path_worksheet = '/dashboard/teacher/worksheets/upload';
 
 export default function TeacherLayoutWrapper({
     children,
 }: {
     children: React.ReactNode;
 }) {
+    const pathname = usePathname();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (pathname !== teacher_path_worksheet) {
+            router.replace(teacher_path_worksheet);
+        }
+    }, [pathname, router]);
+
     return (
         <TeacherLayout title="AssessWise">
             {children}
